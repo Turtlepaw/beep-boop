d7283908152b95f3d375042a6da46db3b619a0cb
Turtlepaw <hi@trtle.xyz>
2022-10-15 02:50:52 +0000

    troubleshooting log

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { StartService } from "./handler";
 import * as dotenv from 'dotenv';
 dotenv.config()
 import "./configuration";
+console.log("TROUBLESHOOTING LOG:".red, `${process.env}`.gray)
 
 const client = new Client({
     intents: [
