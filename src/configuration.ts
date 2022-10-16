5f1f67e9909d3927cf42793d07ad9716ffe7dc37
Turtlepaw <hi@trtle.xyz>
2022-10-16 01:20:53 +0000

    add support for config.json

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,7 +1,23 @@
 import { EmbedBuilder } from "@discordjs/builders";
+const { Token, ClientId } = requireConfigJSON();
 
-export const token = process.env.TOKEN || "";
-export const clientId = process.env.CLIENT_ID || "1028785995337977856";
+function requireConfigJSON() {
+    try {
+        const { token: Token, clientId: ClientId } = require("./config.json");
+        return {
+            Token,
+            ClientId
+        }
+    } catch (e) {
+        return {
+            Token: "aa",
+            ClientId: "aa"
+        }
+    }
+}
+
+export const token = process.env.TOKEN || Token;
+export const clientId = process.env.CLIENT_ID || ClientId;
 export const guildId = "1028789308401918004";
 export const color = "#FF6060";
 
