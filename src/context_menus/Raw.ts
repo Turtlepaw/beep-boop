b8261f618ecc14eb6effb5517715f1f8ab6409fc
TurtlePaw <turtlesimsyt@gmail.com>
2022-11-27 15:13:24 -0500

    add raw context command

--- src/context_menus/Raw.ts
+++ src/context_menus/Raw.ts
@@ -10,6 +10,7 @@ import { ChannelSelectMenu, EmbedFrom, EmbedModal, EmbedModalFields, MessageBuil
 import { generateId } from "../utils/Id";
 import { FindWebhook } from "../utils/Webhook";
 import { GenerateURL, ShortenURL } from "../utils/Discohook";
+import fetch from "node-fetch";
 
 export default class DeleteThis extends ContextMenu {
     constructor() {
@@ -24,8 +25,49 @@ export default class DeleteThis extends ContextMenu {
     }
 
     public async ExecuteContextMenu(interaction: MessageContextMenuCommandInteraction, client: Client) {
-        await interaction.reply({
-            content: `${codeBlock('json', JSON.stringify(interaction.targetMessage.toJSON()))}`
-        })
+        const Message = await interaction.reply({
+            content: `Select a type.`,
+            ephemeral: true,
+            fetchReply: true,
+            components: [
+                new ActionRowBuilder<ButtonBuilder>()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setLabel("Full Raw Message")
+                            .setCustomId("FULL")
+                            .setStyle(ButtonStyle.Primary),
+                        new ButtonBuilder()
+                            .setCustomId("SHORT")
+                            .setLabel("Message Content")
+                            .setStyle(ButtonStyle.Secondary)
+                    )
+            ]
+        });
+
+        const btn = await Message.awaitMessageComponent({
+            componentType: ComponentType.Button,
+            time: 0
+        });
+
+        if (btn.customId == "FULL") {
+            /*const res = await fetch("https://vaultb.in/api/pastes", {
+                method: "post",
+                body: JSON.stringify({
+                    content: interaction.targetMessage.toJSON(),
+                    language: "json"
+                })
+            });
+            //const res = await raw.json();
+            console.log(res)*/
+            await btn.update({
+                content: `Coming soon!`,
+                components: []
+            });
+        } else {
+            await btn.update({
+                content: `${codeBlock(interaction.targetMessage.content)}`,
+                components: []
+            });
+        }
     }
 }
