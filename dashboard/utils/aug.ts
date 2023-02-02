ca7822d032edf8c411681f7ef13caffcfe5c9166
Turtlepaw <hi@trtle.xyz>
2023-02-01 23:09:58 -0500

    Delete aug.ts

--- dashboard/utils/aug.ts
+++
@@ -1,7 +0,0 @@
-import type { DiscordMessageOptions } from '@skyra/discord-components-core/dist/types/options';
-
-declare global {
-    interface Window {
-        $discordMessage: DiscordMessageOptions;
-    }
-}
