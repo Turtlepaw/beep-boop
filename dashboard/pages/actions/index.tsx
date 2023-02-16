1e7e2dcfcb833e6222a9cfa97e23a833bbbbf29e
Turtlepaw <hi@trtle.xyz>
2023-02-16 11:57:26 -0500

    fix path

--- dashboard/pages/actions/index.tsx
+++ dashboard/pages/actions/index.tsx
@@ -9,7 +9,7 @@ import { DefaultProps, parseUser } from '../../utils/parse-user';
 import { Configuration } from '../../utils/configuration';
 import { ButtonStyle } from "../../components/Theming";
 import { Meta } from '../../components/Meta';
-import { BrandColor } from '../../components/Link';
+import { BrandColor } from '../../components/Utils/Link';
 import { CustomBadge, Presets } from '../../components/Common';
 import { Action, GetActions } from '../../utils/api';
 
