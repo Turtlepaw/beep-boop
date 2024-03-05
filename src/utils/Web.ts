775761a27d672b95e9215adc9bb6750f37ed7a18
TurtlePaw <81275769+Turtlepaw@users.noreply.github.com>
2024-03-05 18:17:05 -0500

    feat: fix preview

--- src/utils/Web.ts
+++ src/utils/Web.ts
@@ -5,31 +5,42 @@ const wait = promisify(setTimeout);
 /**
  * Captures a screenshot of the website.
  */
-export async function getScreenshot(browser: puppeteer.Browser, page: string | puppeteer.Page, delay = 0, delayCallback: () => unknown) {
-    if (!browser) browser = await puppeteer.launch();
-    if (page == typeof String && (!page.toString().startsWith("http"))) "http://" + page;
-    if (typeof page == "string") {
-        const pageURL = page;
-        page = await browser.newPage();
-        await page.goto(pageURL);
-    }
-
-    if (delay > 0) await wait(delay);
-    await delayCallback();
-    const buffer = await page.screenshot({
-        omitBackground: true,
-        encoding: 'binary'
+export async function getScreenshot(
+  browser: puppeteer.Browser,
+  page: string | puppeteer.Page,
+  delay = 0,
+  delayCallback: () => unknown
+) {
+  if (!browser)
+    browser = await puppeteer.launch({
+      executablePath: "/usr/bin/chromium-browser",
     });
+  if (page == typeof String && !page.toString().startsWith("http"))
+    "http://" + page;
+  if (typeof page == "string") {
+    const pageURL = page;
+    page = await browser.newPage();
+    await page.goto(pageURL);
+  }
 
-    if (typeof buffer == "string") throw new Error("Screenshot failed: page.screenshot() returned a string");
+  if (delay > 0) await wait(delay);
+  await delayCallback();
+  const buffer = await page.screenshot({
+    omitBackground: true,
+    encoding: "binary",
+  });
 
-    return {
-        buffer,
-        page: typeof page == "string" ? page : page.url()
-    };
+  if (typeof buffer == "string")
+    throw new Error("Screenshot failed: page.screenshot() returned a string");
+
+  return {
+    buffer,
+    page: typeof page == "string" ? page : page.url(),
+  };
 }
 
 export function ResolveURL(url: string) {
-    if (!(url.startsWith("https://") || url.startsWith("http://"))) url = "http://" + url;
-    return url;
+  if (!(url.startsWith("https://") || url.startsWith("http://")))
+    url = "http://" + url;
+  return url;
 }
