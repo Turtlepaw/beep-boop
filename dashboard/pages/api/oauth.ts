c58c62bf12099120c9d2356b2c77d56189f057fa
TurtlePaw <81275769+Turtlepaw@users.noreply.github.com>
2024-03-06 20:05:17 -0500

    Fix api

--- dashboard/pages/api/oauth.ts
+++ dashboard/pages/api/oauth.ts
@@ -10,72 +10,78 @@ const scope = ["identify", "guilds"].join(" ");
 const REDIRECT_URI = `${config.appUri}/api/oauth`;
 
 const OAUTH_QS = new URLSearchParams({
-    client_id: config.clientId,
-    redirect_uri: REDIRECT_URI,
-    response_type: "code",
-    scope,
+  client_id: config.clientId,
+  redirect_uri: REDIRECT_URI,
+  response_type: "code",
+  scope,
 }).toString();
 
 const OAUTH_URI = `https://discord.com/api/oauth2/authorize?${OAUTH_QS}`;
 
 export default async function OAuth(req: NextApiRequest, res: NextApiResponse) {
-    console.log(config)
-    if (req.method !== "GET") return res.redirect("/");
+  console.log(config);
+  if (req.method !== "GET") return res.redirect("/");
 
-    const { code = null, error = null } = req.query;
+  const { code = null, error = null } = req.query;
 
-    if (error) {
-        return res.redirect(`/?error=${req.query.error}`);
+  if (error) {
+    return res.redirect(`/?error=${req.query.error}`);
+  }
+
+  if (!code || typeof code !== "string") return res.redirect(OAUTH_URI);
+
+  const body = new URLSearchParams({
+    client_id: config.clientId,
+    client_secret: config.clientSecret,
+    grant_type: "authorization_code",
+    redirect_uri: REDIRECT_URI,
+    code,
+    scope,
+  }).toString();
+
+  const { access_token = null, token_type = "Bearer" } = (await fetch(
+    "https://discord.com/api/oauth2/token",
+    {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      method: "POST",
+      body,
     }
+  ).then((res) => res.json())) as any;
 
-    if (!code || typeof code !== "string") return res.redirect(OAUTH_URI);
+  if (!access_token || typeof access_token !== "string") {
+    return res.redirect(OAUTH_URI);
+  }
 
-    const body = new URLSearchParams({
-        client_id: config.clientId,
-        client_secret: config.clientSecret,
-        grant_type: "authorization_code",
-        redirect_uri: REDIRECT_URI,
-        code,
-        scope,
-    }).toString();
-
-    const { access_token = null, token_type = "Bearer" } = await fetch("https://discord.com/api/oauth2/token", {
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        method: "POST",
-        body,
-    }).then((res) => res.json());
-
-    if (!access_token || typeof access_token !== "string") {
-        return res.redirect(OAUTH_URI);
+  const me: DiscordUser | { unauthorized: true } = (await fetch(
+    "http://discord.com/api/users/@me",
+    {
+      headers: { Authorization: `${token_type} ${access_token}` },
     }
+  ).then((res) => res.json())) as any;
 
-    const me: DiscordUser | { unauthorized: true } = await fetch("http://discord.com/api/users/@me", {
-        headers: { Authorization: `${token_type} ${access_token}` },
-    }).then((res) => res.json());
+  if (!("id" in me)) {
+    console.log(me);
+    return res.redirect(OAUTH_URI);
+  }
 
-    if (!("id" in me)) {
-        console.log(me)
-        return res.redirect(OAUTH_URI);
-    }
+  const token = sign(me, config.jwtSecret, { expiresIn: "1y" });
 
-    const token = sign(me, config.jwtSecret, { expiresIn: "1y" });
+  //console.log(token);
+  await CreateUser(token, {
+    access_token,
+    token_type,
+    jwt_token: token,
+  });
 
-    //console.log(token);
-    await CreateUser(token, {
-        access_token,
-        token_type,
-        jwt_token: token
-    });
+  res.setHeader(
+    "Set-Cookie",
+    serialize(config.cookieName, token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "lax",
+      path: "/",
+    })
+  );
 
-    res.setHeader(
-        "Set-Cookie",
-        serialize(config.cookieName, token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== "development",
-            sameSite: "lax",
-            path: "/",
-        })
-    );
-
-    res.redirect("/");
-};
+  res.redirect("/");
+}
