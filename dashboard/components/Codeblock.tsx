147a74b4a96d72ddd22a6aa66981e4ccb7f77133
TurtlePaw <81275769+Turtlepaw@users.noreply.github.com>
2024-03-06 20:00:15 -0500

    Fix build bug

--- dashboard/components/Codeblock.tsx
+++ dashboard/components/Codeblock.tsx
@@ -2,18 +2,21 @@ import React from "react";
 import Highlight from "react-highlight";
 
 export interface BasicProperties {
-    children: React.ReactNode;
-    className?: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
 export function Codeblock({ children, className }: BasicProperties) {
-    return (
-        <div className={`bg-[#2f3136] px-2 py-2 border-[#202225] rounded-[.3rem] ${className}`}>
-            <Highlight
-                language="javascript"
-                //@ts-expect-error
-                children={children}
-            />
-        </div>
-    )
+  return (
+    <div
+      className={`bg-[#2f3136] px-2 py-2 border-[#202225] rounded-[.3rem] ${className}`}
+    >
+      {/* @ts-expect-error mismatch with react-highlight? */}
+      <Highlight
+        language="javascript"
+        //@ts-expect-error
+        children={children}
+      />
+    </div>
+  );
 }
